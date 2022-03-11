23ea9fcaaea91f5ed3be552c20e8b4a9c746e265
marcoscrocci <marcoscrocci@gmail.com>
2022-03-10 22:00:25 -0300

    Styled Components - Implementando a alteração de temas - Parte 2 - Pegar a opção light ou dark do sistema automaticamente

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from 'styled-components';
-//import { useColorScheme } from 'react-native'
+import { useColorScheme, Text } from 'react-native'
 
 import { Home } from "./src/screens/Home";
 import themes from './src/themes';
@@ -9,8 +9,8 @@ import themes from './src/themes';
 
 export default function App() {
 
-    //const colorScheme = useColorScheme();
-    const theme = themes.light; //themes[colorScheme] || themes.light;
+    const colorScheme = useColorScheme();
+    const theme = themes[colorScheme === 'dark' ? 'dark' : 'light'];
 
     return (
         <ThemeProvider theme={theme}>
